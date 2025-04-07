a5b2c04b0b9cd5f2e9bac527df0d40799e5cd1bd
JENY\jenpa <deysonurrego159@gmail.com>
2025-04-07 16:24:32 -0500

    Funcionalidad crear usuario

--- empresa/Usuario.php
+++ empresa/Usuario.php
@@ -21,9 +21,23 @@ class Usuario
         // Lógica para obtener un usuario por ID
     }
 
-    public function crearUsuario()
+    public function crearUsuario($datos)
     {
-        // Lógica para insertar usuario
+        $sql = "INSERT INTO usuarios (primer_nombre, segundo_nombre, primer_apellido, segundo_apellido, documento, telefono, correo, fecha_nacimiento, direccion)
+        VALUES (:primer_nombre, :segundo_nombre, :primer_apellido, :segundo_apellido, :documento, :telefono, :correo, :fecha_nacimiento, :direccion)";
+
+        $resultado = $this->conn->prepare($sql);
+        $resultado->bindParam(':primer_nombre', $datos['primer_nombre']);
+        $resultado->bindParam(':segundo_nombre', $datos['segundo_nombre']);
+        $resultado->bindParam(':primer_apellido', $datos['primer_apellido']);
+        $resultado->bindParam(':segundo_apellido', $datos['segundo_apellido']);
+        $resultado->bindParam(':documento', $datos['documento']);
+        $resultado->bindParam(':telefono', $datos['telefono']);
+        $resultado->bindParam(':correo', $datos['correo']);
+        $resultado->bindParam(':fecha_nacimiento', $datos['fecha_nacimiento']);
+        $resultado->bindParam(':direccion', $datos['direccion']);
+
+        return $resultado->execute();
     }
 
     public function actualizarUsuario()
